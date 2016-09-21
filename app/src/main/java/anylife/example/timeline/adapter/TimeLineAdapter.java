10ff573bb7f977b0dbc410da8a33331d04f7ab7d
B5FCAA07.曾刘保 <zenglb@vanke.net.cn>
2016-09-21 19:09:29 +0800

    merge code

--- app/src/main/java/anylife/example/timeline/adapter/TimeLineAdapter.java
+++ app/src/main/java/anylife/example/timeline/adapter/TimeLineAdapter.java
@@ -47,6 +47,17 @@ public class TimeLineAdapter extends RecyclerView.Adapter<TimeLineAdapter.TimeLi
 		return mDataSet.size();
 	}
 
+	public class ItemType {
+		public final static int NORMAL = 0;
+
+		public final static int HEADER = 1;
+		public final static int FOOTER = 2;
+
+		public final static int START = 4;
+		public final static int END = 8;
+		public final static int ATOM = 16;
+	}
+
 
 	public class TimeLineViewHolder extends RecyclerView.ViewHolder {
 		private TextView mName;
